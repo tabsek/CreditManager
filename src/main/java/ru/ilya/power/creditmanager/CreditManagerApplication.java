e1ca1873d949cf20facda2d100f0cc482b6352da
Ilya_Konovalov <ilia.mishchenko03@gmail.com>
2026-06-23 19:00:55 +0300

    feat: initial project setup

---
+++ src/main/java/ru/ilya/power/creditmanager/CreditManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.ilya.power.creditmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ilya/power/creditmanager/CreditManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilya.power.creditmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
