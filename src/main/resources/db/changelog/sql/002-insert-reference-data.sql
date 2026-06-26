253a917d40e26cdfba3894a1b3c718da7bfdac96
Ilya_Konovalov <ilia.mishchenko03@gmail.com>
2026-06-26 14:43:02 +0300

    feat: add reference data

---
+++ src/main/resources/db/changelog/sql/002-insert-reference-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO client_statuses (id, name) VALUES (1, 'ACTIVE');
+INSERT INTO client_statuses (id, name) VALUES (2, 'BLOCKED');
+INSERT INTO client_statuses (id, name) VALUES (3, 'CLOSED');
+
+INSERT INTO loan_statuses (id, name) VALUES (1, 'DRAFT');
+INSERT INTO loan_statuses (id, name) VALUES (2, 'ACTIVE');
+INSERT INTO loan_statuses (id, name) VALUES (3, 'CLOSED');
+INSERT INTO loan_statuses (id, name) VALUES (4, 'OVERDUE');
+INSERT INTO loan_statuses (id, name) VALUES (5, 'CANCELLED');
+
+INSERT INTO currencies (id, code) VALUES (1, 'RUB');
+INSERT INTO currencies (id, code) VALUES (2, 'USD');
+INSERT INTO currencies (id, code) VALUES (3, 'EUR');
